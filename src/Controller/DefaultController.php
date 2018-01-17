5eb92229efc92cd1175af5a6e7d3d889f0c9338e
Joan <joan16v@gmail.com>
2018-01-17 17:16:01 +0100

    psr2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -26,7 +26,7 @@ class DefaultController extends Controller
         $number = mt_rand(0, 100);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky number: ' . $number . '</body></html>'
         );
     }
 }
