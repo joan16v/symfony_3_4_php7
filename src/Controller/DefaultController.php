124fb770a10b54f0ffa0c97ed769a8cae52fb5cb
Joan <joan16v@gmail.com>
2018-01-24 14:32:48 +0100

    ordered libraries

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
