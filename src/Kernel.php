7125a6d52ef554cfe9f4cd86d0fb1d802e0ed5e2
Joan Gimenez Donat <joan16v@gmail.com>
2019-08-13 08:38:05 +0200

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends BaseKernel
             $routes->import($confDir . '/routes/*' . self::CONFIG_EXTS, '/', 'glob');
         }
         if (is_dir($confDir.'/routes/'.$this->environment)) {
-            $routes->import($confDir . '/routes/' . $this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
+            $routes->import($confDir . '/routes/' . $this->environment.'/**/*' . self::CONFIG_EXTS, '/', 'glob');
         }
         $routes->import($confDir . '/routes'.self::CONFIG_EXTS, '/', 'glob');
     }
